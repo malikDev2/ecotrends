c1193347549c2e906bf5808e2a5cb045b777e0cf
Malik <malik.h.jacobs@outlook.com>
2025-08-08 11:29:30 -0400

    added r images

--- react/my-app/src/pages/home.tsx
+++ react/my-app/src/pages/home.tsx
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom';
+import Eco from "../assets/eco.png";
+import Aff from '../assets/afford.png';
+import Ine from "../assets/ineq.png";
 
 function Home() {
     return(<>
@@ -6,17 +9,17 @@ function Home() {
     <div className="homedivs">
         <Link to="affordability" style={{textDecoration: 'None'}}>
             <div className="home"  style={{background: 'rgba(255, 193, 24, 1)'}} >
-                <h2>Affordability</h2>
+                <img src={Aff} alt="Affordability" />
             </div>
         </Link>
         <Link to="economy" style={{textDecoration: 'None'}}>
             <div className="home" style={{background: 'rgba(0, 241, 48, 1)'}}>
-                <h2>Economy</h2>
+                <img src={Eco} alt="Economy" />   
             </div>
         </Link>
         <Link to="inequality" style={{textDecoration: 'None'}}>
             <div className="home" style={{background: 'rgba(102, 0, 146, 0.56)'}}>
-                <h2>Inequality</h2>
+                <img src={Ine} alt="Inequality" />
             </div>
         </Link>
 
